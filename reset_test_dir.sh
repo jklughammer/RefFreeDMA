eda1c9ab1c336095f4f9b34a79fae2adb5d54705
jklughammer <jklughammer@cemm.oeaw.ac.at>
2016-02-19 18:31:06 +0100

    make reset test_dir more secure

--- reset_test_dir.sh
+++ reset_test_dir.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+set -e
 dir=$1
 
 shopt -s extglob
 
 read -p "ATTENTION:THIS WILL WIPE THE INDICATED DIRECTORY. Do you really want to reset the directory $dir (y/n)? " RESP
 if [ $RESP = "y" ]; then
-  cd $dir
+  cd $dir 
   rm -r !(meta|unmapped_bam) 2>/dev/null
   rm *.done 2>/dev/null
   echo "$dir is now reset."
