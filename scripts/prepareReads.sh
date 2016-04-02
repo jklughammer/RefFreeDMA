79ca03d7edbb578691b8845f4726d5cfd3c8ec86
jklughammer <jklughammer@cemm.oeaw.ac.at>
2016-04-02 11:03:12 +0200

    add reporting of enzyme specific read counts

--- scripts/prepareReads.sh
+++ scripts/prepareReads.sh
@@ -14,6 +14,7 @@ trim_galore_path=$5
 cutadapt_path=$6
 nameSeparator=$7
 resMotifs=$8
+samtools_path=$9
 #-----------------------PATHS_END----------------------------
 
 
@@ -91,9 +92,41 @@ convMotifs=`echo $resMotifs| awk '{gsub(/C/,"T");gsub("[|]","|^");print "^"$1}'`
 echo $convMotifs
 paste <(awk 'NR%4==1' $trimmed_fastq) <(awk 'NR%4==2' $trimmed_fastq) <(awk 'NR%4==2 {gsub(/C/,"T",$1);print $1}' $trimmed_fastq)|awk -v m=$convMotifs '$3 ~ m && $2 !~ /N/'|sort -k 2,2 | uniq -f 1 > $working_dir/reduced/$(basename $out_fastq .fastq)_uniq.ref || exit 1
 
-
-
 echo "" > $working_dir/$new_name.done
+
+#now check restriction digest efficiency (this is independent of the rest of the pipeline)
+total_reads=`$samtools_path/samtools view $in_file|wc -l`
+echo "total_reads\t$total_reads" > $working_dir/fastq/$new_name.stats
+
+IFS='|' read -a resMotif_array <<<"$resMotifs"
+
+all_num="counts"
+all_motifs="motifs"
+all_sum=0
+for motif in "${resMotif_array[@]}";do 
+	motifComplete="${motif/C/[CT]}"
+	motifT="${motif/C/T}"
+num=`$samtools_path/samtools view $in_file|grep -P "\t$motifComplete"|wc -l`
+
+num_c=`$samtools_path/samtools view $in_file|grep -P "\t$motif"|wc -l`
+num_t=`$samtools_path/samtools view $in_file|grep -P "\t$motifT"|wc -l`
+
+perc_c=`printf "%.2f" $(echo "100*$num_c/$total_reads"|bc -l)`
+perc_t=`printf "%.2f" $(echo "100*$num_t/$total_reads"|bc -l)`
+
+all_sum=`echo "$all_sum+$num"|bc`
+
+all_num="$all_num\t$num\t$perc_c\t$perc_t"
+all_motifs="$all_motifs\t$motifComplete\t$motif\t$motifT"
+done
+
+all_sum_perc=`printf "%.2f" $(echo "100*($total_reads-$all_sum)/$total_reads"|bc -l)`
+
+all_num="$all_num\t$all_sum_perc"
+all_motifs="$all_motifs\tothers"
+
+echo $all_motifs>> $working_dir/fastq/$new_name.stats
+echo $all_num>> $working_dir/fastq/$new_name.stats
 
 
 
